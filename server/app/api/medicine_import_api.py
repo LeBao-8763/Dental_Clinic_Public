9968597c5cf57da0a9c66e3f7f70adf095776ad9
Shadow-Garden8763 <letrangiabao283@gmail.com>
2025-12-01 16:59:11 +0700

    xong be thêm loại thuốc, nhập lô thuốc

---
+++ server/app/api/medicine_import_api.py
@@ -0,0 +1,33 @@
+from flask import request
+from flask_restx import Resource
+from app.api_conf import medicine_import_ns, medicine_import_model, medicine_import_parser
+from app.dao import dao_medicine_import, dao_medicine
+
+
+# from flask_jwt_extended import jwt_required, get_jwt_identity
+
+@medicine_import_ns.route('/')
+class MedicineImportList(Resource):
+    @medicine_import_ns.marshal_list_with(medicine_import_model)
+    def get(self):
+        "Lấy danh sách các loại thuốc"
+        return dao_medicine_import.get_medicine_import_list()
+
+    @medicine_import_ns.expect(medicine_import_parser, validate=True)
+    @medicine_import_ns.marshal_with(medicine_import_model, code=201)
+    #@jwt_required()  # nếu muốn chỉ admin/staff
+    def post(self):
+        "Nhập thuốc vào kho"
+        data = medicine_import_parser.parse_args()
+
+        new_import = dao_medicine_import.import_medicine(
+            user_id=data['user_id'],
+            medicine_id=data['medicine_id'],
+            quantity=data['quantity_imported'],
+            price=data['price']
+        )
+
+        if new_import:
+            return new_import, 201
+
+        return {'message': 'Không tìm thấy thuốc'}, 404
---
+++ server/app/dao/dao_medicine_import.py
@@ -0,0 +1,29 @@
+from app import db
+from app.models import Medicine, MedicineImport
+from datetime import datetime
+
+# Nhập thuốc vào kho
+def import_medicine(user_id, medicine_id, quantity, price):
+    med = Medicine.query.get(medicine_id)
+    if not med:
+        return None
+
+    # Tạo bản ghi nhập thuốc
+    import_record = MedicineImport(
+        user_id=user_id,
+        medicine_id=medicine_id,
+        import_date=datetime.utcnow(),
+        quantity_imported=quantity,
+        price=price,
+        stock_quantity=med.stock_quantity + quantity
+    )
+    db.session.add(import_record)
+
+    # Cập nhật tồn kho
+    med.stock_quantity += quantity
+    db.session.commit()
+    return import_record
+
+# Lấy danh sách nhập thuốc
+def get_medicine_import_list():
+    return MedicineImport.query.all()
